c34c404a61041dbe1403883c49949b4415f1ed5d
mitsubosh <yuya.mitsuboshi@gmail.com>
2019-12-08 00:01:57 +0900

    add `# frozen_string_literal: true`

--- lib/yamcha.rb
+++ lib/yamcha.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require "yamcha/version"
 
 module Yamcha
--- spec/yamcha_spec.rb
+++ spec/yamcha_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 RSpec.describe Yamcha do
   it "has a version number" do
     expect(Yamcha::VERSION).not_to be nil
